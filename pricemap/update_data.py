da07e813539bbc59b97bb0596e28e28baf2da978
mohamed lajmi <mohamed.lajmi@gmail.com>
2022-02-26 10:33:37 +0100

    new update implementation

--- pricemap/update_data.py
+++ pricemap/update_data.py
@@ -50,8 +50,9 @@ def init_database():
             price INTEGER,
             area INTEGER,
             room_count INTEGER,
-            seen_at TIMESTAMP,
-            PRIMARY KEY (id, seen_at)
+            first_seen_at TIMESTAMP,
+            last_seen_at TIMESTAMP,
+            PRIMARY KEY (id)
         );
     """
     cursor = g.db.cursor(cursor_factory=psycopg2.extras.DictCursor)
@@ -124,7 +125,8 @@ def update():
             for item in response.json():
                 listing = decode_item(item)
                 listing["geom"] = geom_id
-                listing["seen_at"] = datetime.now()
+                listing["first_seen_at"] = datetime.now()
+                listing["last_seen_at"] = datetime.now()
                 listings.append(listing)
 
     logging.error(f"listings: {listings}")
@@ -136,117 +138,28 @@ def update():
                     %(price)s,
                     %(area)s,
                     %(room_count)s,
-                    %(seen_at)s
+                    %(first_seen_at)s,
+                    %(last_seen_at)s
                 )
         """
 
     psycopg2.extras.execute_batch(db_cursor, query, listings, page_size=100)
-
+    g.db.commit()
     return
 
-    for listing in listings:
-        logging.error(f"insert query: {listing['listing_id']}")
-        db_cursor.execute(
-            query,
-            {
-                "listing_id": listing["listing_id"],
-                "geom": listing["geom"],
-                "price": listing["price"],
-                "area": listing["area"],
-                "room_count": listing["room_count"],
-                "seen_at": listing["seen_at"],
-            },
+    query = f"""
+        INSERT INTO listings VALUES(
+            %(listing_id)s,
+            %(geom)s,
+            %(price)s,
+            %(area)s,
+            %(room_count)s,
+            %(first_seen_at)s,
+            %(last_seen_at)s
         )
-        g.db.commit()
-    return
-    for geom in GEOMS_IDS:
-        p = 0
-        while True:
-            p += 1
-            url = "http://listingapi:5000/listings/" + str(geom) + "?page=" + str(p)
-            d = requests.get(url)
-
-            # Break when finished
-            if d.status_code == 416:
-                break
-
-            for item in d.json():
-                listing_id = item["listing_id"]
-                try:
-                    room_count = (
-                        1
-                        if "Studio" in item["title"]
-                        else int(
-                            "".join(
-                                [
-                                    s
-                                    for s in item["title"].split("pièces")[0]
-                                    if s.isdigit()
-                                ]
-                            )
-                        )
-                    )
-                except:
-                    room_count = 0
-
-                try:
-                    price = int("".join([s for s in item["price"] if s.isdigit()]))
-                except:
-                    price = 0
-
-                try:
-                    area = int(
-                        item["title"]
-                        .split("-")[1]
-                        .replace(" ", "")
-                        .replace("\u00a0m\u00b2", "")
-                    )
-                except:
-                    area = 0
-
-                seen_at = datetime.now()
-
-                now = datetime.now()
-
-                query = f"""
-                    INSERT INTO listings VALUES(
-                        %(listing_id)s,
-                        %(geom)s,
-                        %(price)s,
-                        %(area)s,
-                        %(room_count)s,
-                        %(first_seen_at)s,
-                        %(last_seen_at)s
-                    )
-                    ON CONFLICT (id) DO UPDATE 
-                    SET last_seen_at = %(last_seen_at)s;
-                """
-
-                db_cursor.execute(
-                    query,
-                    {
-                        "listing_id": listing_id,
-                        "geom": geom,
-                        "price": price,
-                        "area": area,
-                        "room_count": room_count,
-                        "first_seen_at": now,
-                        "last_seen_at": now,
-                    },
-                )
-
-                sql = f"""
-                    INSERT INTO listings VALUES(
-                        {listing_id},
-                        {geom},
-                        {price},
-                        {area},
-                        {room_count},
-                        '{seen_at}'
-                    );
-                """
-                db_cursor.execute(sql)
-                g.db.commit()
+        ON CONFLICT (id) DO UPDATE 
+        SET last_seen_at = %(last_seen_at)s;
+    """
 
 
 def update_old():
